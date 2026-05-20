6f2097e51eb8ed811ef3850597ee6cc26e87c274
yothuany <celestinothauany@gmail.com>
2026-05-19 22:53:39 -0400

    add estrutura base controllers, services, models e DTOs

--- obj/Debug/net8.0/BiblioSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/BiblioSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiblioSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98f857bdc933e02a42dc6a08c578efddb8964c9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiblioSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiblioSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
